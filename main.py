e82679e89e49689358b8350497aef1f5a98da029
sudeepchandra31 <dorbalasudeepchandra@gmail.com>
2026-09-19 10:47:53 +0530

    Update CORS for deployed frontend

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://jarvissgo.netlify.app", "http://localhost:5173"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
